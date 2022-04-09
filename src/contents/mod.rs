48997961abed60bac7d34ac444a632ff050d5d8d
Yoshinori Tanimura <tanimura@ricos.co.jp>
2022-04-09 19:51:12 +0900

    hide doc mode

--- src/contents/mod.rs
+++ src/contents/mod.rs
@@ -3,6 +3,8 @@ use crate::*;
 #[derive(Clone, Debug, Default)]
 pub struct Contents {
     div: NodeRef,
+    hide_doc: NodeRef,
+    view_doc: NodeRef,
 }
 
 #[derive(Clone, Debug, PartialEq, Properties)]
@@ -34,7 +36,33 @@ impl Component for Contents {
     }
 
     fn view(&self, _: &Context<Self>) -> Html {
-        html! { <div class="outer_box"><div class="contents" ref={ self.div.clone() } /></div> }
+        let outer0 = NodeRef::default();
+        let outer1 = outer0.clone();
+        let hide_doc0 = self.hide_doc.clone();
+        let hide_doc1 = self.hide_doc.clone();
+        let view_doc0 = self.view_doc.clone();
+        let view_doc1 = self.view_doc.clone();
+        html! {
+            <>
+            <div class="outer_box" ref={ outer0.clone() }><div class="contents" ref={ self.div.clone() } /></div>
+            <img src="./hidedoc.svg" class="docswitch" hidden=false ref={ self.hide_doc.clone() }
+                onclick= { move |_| {
+                    let outer0 = outer0.cast::<HtmlDivElement>().unwrap();
+                    outer0.style().set_property("display", "none").unwrap();
+                    outer0.set_hidden(true);
+                    hide_doc0.cast::<HtmlImageElement>().unwrap().set_hidden(true);
+                    view_doc0.cast::<HtmlImageElement>().unwrap().set_hidden(false);
+                } } />
+            <img src="./viewdoc.svg" class="docswitch" hidden=true ref={ self.view_doc.clone() }
+                onclick= { move |_| {
+                    let outer1 = outer1.cast::<HtmlDivElement>().unwrap();
+                    outer1.style().set_property("display", "block").unwrap();
+                    outer1.set_hidden(false);
+                    hide_doc1.cast::<HtmlImageElement>().unwrap().set_hidden(false);
+                    view_doc1.cast::<HtmlImageElement>().unwrap().set_hidden(true);
+                } } />
+            </>
+        }
     }
 
     fn rendered(&mut self, ctx: &Context<Self>, _: bool) {
